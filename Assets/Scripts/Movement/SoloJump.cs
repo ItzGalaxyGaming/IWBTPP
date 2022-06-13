a99ac805826b7f588d6d77c2bf93c16f7e9a6adc
DetectiveGato <gatoxd5@gmail.com>
2022-06-13 14:58:32 -0400

    Juego implementado
    Se agregó el juego

---
+++ Assets/Scripts/Movement/SoloJump.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SoloJump : MonoBehaviour
+{
+    private float horizontal;
+    public float jumpingPower = 16f;
+   
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+}
+
